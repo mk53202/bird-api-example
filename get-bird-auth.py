60ba055ddd6581101351a1d32d384dfc966e47d5
Matt Koster <mattkoster@gmail.com>
2018-07-10 11:22:59 -0500

    Added inital get-bird-auth.py example to be used by show-birds.py

---
+++ get-bird-auth.py
@@ -0,0 +1,17 @@
+import requests
+
+url = "https://api.bird.co/user/login"
+
+body_payload = "{\"email\": \"yourmom@aol.com\"}" # Use unique email!
+device_GUID = "12345678-12ab-34cd-56ef-fhdhfdjh8w8d"
+
+headers = {
+    'Platform': "ios",
+    'Device-id': device_GUID,
+    'Content-type': "application/json",
+    'Cache-Control': "no-cache"
+}
+
+response = requests.request("POST", url, data=body_payload, headers=headers)
+
+print(response.text) # If an unique email is used, it will return a token for the GET request in show-birds.py
